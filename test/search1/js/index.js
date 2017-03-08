68f5add35079d081b546fa8146bda4c4f041a60f
ruservis <brewbaker@yandex.ru>
2017-03-08 17:10:53 +0300

    Add files via upload

---
+++ test/search1/js/index.js
@@ -0,0 +1,318 @@
+var data1 = {"0211":{"categories":["Dept. Stores and Supermarkets"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 3 / NIVEAU 3\r\n","name":"Canadian Tire","phone":"416-979-2025"},"0301":{"categories":["Electronics, Computers and Telephones"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 3 / NIVEAU 3\r\n","name":"Best Buy","phone":"416-642-8321"},"0303":{"categories":["Specialty Food"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 3 / NIVEAU 3\r\n","name":"Starbucks Coffee","phone":"416-506-1602"},"1105":{"categories":["Unisex Apparel"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 1 / NIVEAU 1\r\n","name":"American Eagle Outfitters","phone":"416-204-9557"},"1108":{"categories":["Electronics, Computers and Telephones","Music & Videos"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 1 / NIVEAU 1\r\n","name":"HMV","phone":"416-340-9801"},"1113":{"categories":["Electronics, Computers and Telephones"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 1 / NIVEAU 1\r\n","name":"Rogers Plus Store","phone":"416-351-1522"},"1114":{"categories":["Electronics, Computers and Telephones"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 1 / NIVEAU 1\r\n","name":"The Source","phone":"416-979-7776"},"2104":{"categories":["Footwear"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 2 / NIVEAU 2\r\n","name":"Capezio","phone":"416-597-6662"},"2108":{"categories":["Children's Apparel"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 2 / NIVEAU 2\r\n","name":"Baby Gap","phone":"416-591-0512"},"2111":{"categories":["Footwear"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 2 / NIVEAU 2\r\n","name":"Rockport","phone":"416-597-0646"},"2112":{"categories":["Footwear"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 2 / NIVEAU 2\r\n","name":"Geox","phone":"416-599-4369"},"2113":{"categories":["Fashion Accessories"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 2 / NIVEAU 2\r\n","name":"Sunglass Hut","phone":"416-971-6395"},"2117":{"categories":["Footwear"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 2 / NIVEAU 2\r\n","name":"Town Shoes","phone":"416-979-9914"},"2124":{"categories":["Unisex Apparel"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 2 / NIVEAU 2\r\n","name":"Bench","phone":"416-548-8656"},"2125":{"categories":["Ladies Apparel"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 2 / NIVEAU 2\r\n","name":"Marciano","phone":"416-598-4676"},"2126":{"categories":["Unisex Apparel"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 2 / NIVEAU 2\r\n","name":"Guess?","phone":"416-598-4676"},"2127":{"categories":["Footwear"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 2 / NIVEAU 2\r\n","name":"Little Burgundy","phone":"416-260-0497"},"2134":{"categories":["Cards, Stationery and Gifts","Jewellery"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 2 / NIVEAU 2\r\n","name":"Swarovski","phone":"647-439-8584"},"3102":{"categories":["Ladies Apparel","Men's Apparel"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 3 / NIVEAU 3\r\n","name":"J. Crew","phone":"416-977-0941"},"3127":{"categories":["Leather & Luggage"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 3 / NIVEAU 3\r\n","name":"Coach","phone":"416-977-3090"},"3131":{"categories":["Health and Beauty"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 3 / NIVEAU 3\r\n","name":"Sephora","phone":"416-595-7227"},"9991":{"categories":["Personal Care & Health Service"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 4/ NIVEAU 4 \r\n","name":"Strategic Health Chiropractor"},"9999":{"categories":["Services"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 4/ NIVEAU 4 \r\n","name":"Dollar Thrifty","phone":"416-591-0861"},"0002A":{"categories":["Sporting Goods & Athleticwear"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 2 / NIVEAU 2\r\n","name":"Atmosphere","phone":"416-260-1735"},"1101A":{"categories":["Specialty Apparel"],"hours":{"Friday":["10:00","9:30"],"Monday":["10:00","9:30"],"Saturday":["10:00","9:30"],"Sunday":["10:00","9:30"],"Thursday":["10:00","9:30"],"Tuesday":["10:00","9:30"],"Wednesday":["10:00","9:30"]},"location":"LEVEL 1 / NIVEAU 1\r\n","name":"La Senza","phone":"416-977-6258"}
+  
+  
+  
+};
+
+
+var storesURL  =  "https://madapp.firebaseio.com/CF.json";
+var storeData;
+
+  init = function (){
+   /*   $.ajax({
+                 url: storesURL,
+                 type: "GET",
+                 accept :"application/json",
+                 dataType: "json",
+                 crossDomain: true
+       })
+      .done(function(data){
+        
+        storeData = data;
+       */ 
+        storeData  = data1;
+        
+        /** Temp : data need to b sorted **/
+        storeData =  filter({value:''});
+        
+        initLetters();
+        initCategories();
+
+        
+        renderList (storeData);
+        renderQuickLinks (storeData);
+               
+    /*  }); */
+     
+};
+
+
+
+
+
+filter = function (param) {
+    
+  var config = {
+    property: "name, categories",
+    wrapper: true,
+    value: '',
+    checkContains: true,
+    startsWith: false,
+    matchCase: false,
+    avoidDuplicates: true,
+    sort: true,
+   /* sortOrder: "desc" */
+  };
+  $.extend (config, param);
+      
+  console.log("Config: ", config);
+     
+   var o = $.fn.filterJSON(storeData, config).toArray();
+  //console.log(o);
+  
+	return o;
+  
+  
+};
+
+
+
+
+renderList = function (data){
+  
+  var source   = $("#store-template").html();
+  var template = Handlebars.compile(source);
+  var html     = template(data);
+  
+  $('#storelist').html(html);
+
+  //console.log(html);
+  
+};
+
+function unique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+var letters = [];
+
+initLetters = function () {
+  $.each(storeData, function(k,v){
+    if(typeof v.name != 'undefined'){
+       letters.push(v.name[0].toUpperCase());
+    }
+  });
+  
+  letters = letters.filter(unique);
+  letters.sort();
+  console.log(letters);
+};
+  
+var categories = [];
+
+initCategories = function () {
+  $.each(storeData, function(k,v){
+    if(typeof v.categories != 'undefined'){
+      
+      $.each(v.categories, function (k1, v1) {
+       categories.push(v1.toUpperCase());
+      });
+      
+    }
+  });
+  
+  categories = categories.filter(unique);
+  categories.sort();
+  console.log(categories);
+};
+
+
+renderQuickLinks = function (data) {
+ 
+  var source   = $("#quicklinks-template").html();
+  var template = Handlebars.compile(source);
+  var html     = template(letters);
+  
+  $('#quicklinks').html(html);
+  
+};
+
+
+$(function(){
+  
+  init();
+  
+  
+  $('#search').on("keyup", function(){
+    
+   var val =  $(this).val();
+   var result =  filter( {value: val});
+ 	 renderList (result);
+    
+  });
+  
+   $('#quicklinks').on("click", "a", function(){
+    
+     var val =  $(this).data('key');
+     var result =  filter( {value: val, property: 'name', startsWith: true, checkContains: false});
+
+     console.log(result);
+     renderList (result);
+
+  });
+  
+    $('#view').on("click", "a", function(){
+     var key =  $(this).data('key');
+     $("#storelist").removeClass().addClass(key);
+  });
+  
+  
+  
+     $('.filter').on("click", "a", function(){
+    
+     var key =  $(this).data('key');
+     var result =  filter( { property: key });
+
+     renderList (result);
+
+  });
+  
+});
+
+
+
+
+
+Handlebars.registerHelper('eachByName', function(context, options) {
+  
+  
+
+  /*** taken from Handlebar each ***/
+   var contextPath;
+      if (options.data && options.ids) {
+        contextPath = Utils.appendContextPath(options.data.contextPath, options.ids[0]) + '.';
+      }
+  
+  	  function blockParams(params, ids) {
+         params.path = ids;
+         return params;
+  		}
+  
+  		
+  		var letter = '#'; /* subin */
+
+      var fn = options.fn, inverse = options.inverse;
+      var i = 0, ret = "", data;
+      function execIteration(key, i, last) {
+        if (data) {
+          data.key = key;
+          data.index = i;
+          data.first = i === 0;
+          data.last  = !!last;
+
+          if (contextPath) {
+            data.contextPath = contextPath + key;
+          }
+        }
+        
+        /* subin */
+        if( letter !== context[key].name[0]){
+          letter = context[key].name[0];
+        	 ret = ret + "<div class='stone'><span>" + context[key].name[0] + "</span></div>";
+        }
+        /* end subin */
+        
+        ret = ret + fn(context[key], {
+          data: data,
+          blockParams: blockParams([context[key], key], [contextPath + key, null])
+        });
+      }
+  
+  
+  
+        var priorKey;
+        for(var key in context) {
+            if(context.hasOwnProperty(key)) {
+              if (priorKey) {
+                execIteration(priorKey, i-1);
+              }
+              priorKey = key;
+              i++;
+            }
+          }
+          if (priorKey) {
+            execIteration(priorKey, i-1, true);
+          }
+  
+  				if(i === 0){
+        		ret = inverse(this);
+      		}
+  			
+        return ret;
+
+});
+
+
+
+
+Handlebars.registerHelper('eachByCategory', function(context, options) {
+  
+  
+
+  /*** taken from Handlebar each ***/
+   var contextPath;
+      if (options.data && options.ids) {
+        contextPath = Utils.appendContextPath(options.data.contextPath, options.ids[0]) + '.';
+      }
+  
+  	  function blockParams(params, ids) {
+         params.path = ids;
+         return params;
+  		}
+  
+  		
+  		var letter = '#'; /* subin */
+
+      var fn = options.fn, inverse = options.inverse;
+      var i = 0, ret = "", data;
+      function execIteration(key, i, last) {
+        if (data) {
+          data.key = key;
+          data.index = i;
+          data.first = i === 0;
+          data.last  = !!last;
+
+          if (contextPath) {
+            data.contextPath = contextPath + key;
+          }
+        }
+        
+        console.log(context[key].categories[0]);
+        
+        /* subin */
+        
+        if( letter !== context[key].categories[0]){
+          letter = context[key].categories[0];
+        	 ret = ret + "<div class='stone'><span>" + context[key].categories[0] + "</span></div>";
+        }
+        /* end subin */
+        
+        ret = ret + fn(context[key], {
+          data: data,
+          blockParams: blockParams([context[key], key], [contextPath + key, null])
+        });
+      }
+  
+  
+  
+        var priorKey;
+        for(var key in context) {
+            if(context.hasOwnProperty(key)) {
+              if (priorKey) {
+                execIteration(priorKey, i-1);
+              }
+              priorKey = key;
+              i++;
+            }
+          }
+          if (priorKey) {
+            execIteration(priorKey, i-1, true);
+          }
+  
+  				if(i === 0){
+        		ret = inverse(this);
+      		}
+  			
+        return ret;
+
+});
